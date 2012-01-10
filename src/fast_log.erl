db945a5d8ed2d5a2a2582abb2213face4224fbea
Seth Falcon <seth@userprimary.net>
2012-01-09 21:56:39 -0800

    Add support for logging proplists

--- src/fast_log.erl
+++ src/fast_log.erl
@@ -1,7 +1,8 @@
 %% -*- erlang-indent-level: 4;indent-tabs-mode: nil; fill-column: 92 -*-
 %% ex: ts=4 sw=4 et
 %% @author Kevin Smith <kevin@opscode.com>
-%% @copyright Copyright 2011 Opscode Inc.
+%% @author Seth Falcon <seth@opscode.com>
+%% @copyright Copyright 2011-2012 Opscode Inc.
 
 -module(fast_log).
 
@@ -11,12 +12,16 @@
 
 %% API
 -export([start_link/1,
+         debug/2,
          debug/3,
          debug/4,
+         info/2,
          info/3,
          info/4,
+         warn/2,
          warn/3,
          warn/4,
+         err/2,
          err/3,
          err/4]).
 
@@ -47,6 +52,9 @@ add_handler(LogConfig) ->
     Name = proplists:get_value(name, LogConfig),
     gen_event:add_handler(Name, ?MODULE, [LogConfig]).
 
+debug(Name, PList) ->
+    send_event(Name, {debug, plist2iolist(PList)}).
+
 debug(Name, Token, Args) ->
     Msg = format_msg("~s ~s", [Token, Args]),
     send_event(Name, {debug, Msg}).
@@ -55,6 +63,17 @@ debug(Name, Token, Format0, Args) ->
     Format = "~s " ++ Format0,
     Msg = format_msg(Format, [Token|Args]),
     send_event(Name, {debug, Msg}).
+
+%% @doc Log a proplist as an INFO message.
+%%
+%% All of the arity two logging functions taking a `PList' argument expect a list of
+%% key/value pairs that will be formated as `K1=V1; K2=V2'. An attempt is made to transform
+%% Keys and values into iolists. In particular, atoms, integers, and floats are
+%% supported. You can also specify a `{Fmt, Args}' tuple which will be passed to
+%% `io_lib:format/2'.
+%%
+info(Name, PList) ->
+    send_event(Name, {info, plist2iolist(PList)}).
 
 info(Name, Token, Args) ->
     Msg = format_msg("~s ~s", [Token, Args]),
@@ -65,6 +84,9 @@ info(Name, Token, Format0, Args) ->
     Msg = format_msg(Format, [Token|Args]),
     send_event(Name, {info, Msg}).
 
+warn(Name, PList) ->
+    send_event(Name, {info, plist2iolist(PList)}).
+
 warn(Name, Token, Args) ->
     Msg = format_msg("~s ~s", [Token, Args]),
     send_event(Name, {warn, Msg}).
@@ -73,6 +95,9 @@ warn(Name, Token, Format0, Args) ->
     Format = "~s " ++ Format0,
     Msg = format_msg(Format, [Token|Args]),
     send_event(Name, {warn, Msg}).
+
+err(Name, PList) ->
+    send_event(Name, {err, plist2iolist(PList)}).
 
 err(Name, Token, Args) ->
     Msg = format_msg("~s ~s", [Token, Args]),
@@ -158,3 +183,35 @@ maybe_check(Name, CheckProb, Config, Overloaded) when CheckProb >= 0.7 ->
     end;
 maybe_check(_Name, _CheckProb, _Config, _Overloaded) ->
     false.
+
+%% Take a proplist of key/value pairs and turn it into an iolist using a `K1=V1; K2;V2'
+%% format. An attempt is made to transform Keys and values into iolists. Atoms, binaries,
+%% strings, integers and floats will all be converted. You can also specify either as `{Fmt,
+%% Args}' and an appropriate call to `io_lib:format(Fmt, Args)' will be made on your behalf
+%% (a bill will be sent to your process' mailbox). The order of the input proplist is
+%% preserved. You can also request pretty-print style formatting (no newlines) by passing
+%% `{raw, Term}'.
+plist2iolist([{_Key, _Val} | _Rest]=PList) ->
+    plist2iolist(PList, []).
+
+plist2iolist([{Key, Val} | Rest], Acc) ->
+    plist2iolist(Rest, [<<"; ">>, [as_io(Key), <<"=">>, as_io(Val)]|Acc]);
+plist2iolist([], [<<"; ">>|Acc]) ->
+    lists:reverse(Acc).
+
+%% Convert input to iolist. Handles atoms, lists, binaries, integers, floats, and tuples of
+%% the form `{Fmt, Args}' suitable for passing to `io_lib:format/2'. Tuples marked as `{raw,
+%% term()}' will be formated using `"~256P"'.
+as_io(X) when is_atom(X) ->
+    erlang:atom_to_binary(X, utf8);
+as_io(X) when is_binary(X); is_list(X) ->
+    X;
+as_io(X) when is_integer(X) ->
+    integer_to_list(X);
+as_io(X) when is_float(X) ->
+    io_lib:format("~f", [X]);
+as_io({raw, X}) ->
+    %% this is last-ditch effort, but may give acceptable results.
+    io_lib:format("~256P", [X, 100]);
+as_io({Fmt, Args}) ->
+    io_lib:format(Fmt, Args).
